d0780c80cd279af761a67d5b593e373de6e7355e
Hosoy <hosoyama@gmail.com>
2019-06-18 20:43:06 +0900

    build

--- docs/precache-manifest.270aacc13d38e3bd73ea800c2a272968.js
+++ docs/precache-manifest.270aacc13d38e3bd73ea800c2a272968.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "53d5b325885e448cfbd3b76b0bfc86c3",
+    "revision": "eeee2ca4a3a536e36a6fd0a67ff82350",
     "url": "./index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/css/2.c4b29727.chunk.css"
   },
   {
-    "revision": "a828d7cb116f0670b4a1",
+    "revision": "ae98100180dca19aa144",
     "url": "./static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.8fd7bff7.chunk.js"
   },
   {
-    "revision": "a828d7cb116f0670b4a1",
-    "url": "./static/js/main.256e12f6.chunk.js"
+    "revision": "ae98100180dca19aa144",
+    "url": "./static/js/main.8fbfac82.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.816505929f79f7476f9e7ea71d5ede45.js"
+  "./precache-manifest.270aacc13d38e3bd73ea800c2a272968.js"
 );
 
 self.addEventListener('message', (event) => {
